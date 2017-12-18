91f79e65ae2597012ae71f3cd77f80dfd80d9b72
MONTES Yannick <montes.yannick@gmail.com>
2017-12-18 14:47:21 -0500

    Put only server for heroku deply

--- client/src/middleware/MapSaga.js
+++
@@ -1,8 +0,0 @@
-import {take, fork} from 'redux-saga/effects';
-import axios from 'axios';
-import {store} from '../store';
-import {LOGIN_REQUEST} from "../actions/opLogin";
-import {CHANGE_MAP_CENTER, CHANGE_MARKER_GEOLOCATION} from "../actions/opMap";
-
-export function* MapFlow() {
-}
--- client/src/util/GoogleMapsWrapper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { GoogleMap,withGoogleMap,withScriptjs } from 'react-google-maps';
-
-const GoogleMapsWrapper = withScriptjs(withGoogleMap(props => {
-    return <GoogleMap {...props} onIdle={props._onIdleChanged}
-                      ref={props._onMapMounted}>{props.children}</GoogleMap>
-}));
-
-export default GoogleMapsWrapper;
