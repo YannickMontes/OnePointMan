494792cf05e025e8bd80e322ee8501eb481fd5f1
mony2701 <yannick.montes@usherbrooke.ca>
2017-12-19 12:57:33 -0500

    Socket heroku

--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ const mapSocketUser = new Map();
 const ADD_GROUP_NOTIFICATION_TYPE = 'ADDED_TO_GROUP';
 const REMOVE_GROUP_NOTIFICATION_TYPE = 'REMOVED_FROM_GROUP';
 
-socket.listen(5000, () =>{
+socket.listen(21, () =>{
 	console.log("Socket listening on port 5000");
 });
 
