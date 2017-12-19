81a87767a65c6f7e1f975c71e465438db101fd04
mony2701 <yannick.montes@usherbrooke.ca>
2017-12-19 12:40:41 -0500

    Socket heroku

--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ const mapSocketUser = new Map();
 const ADD_GROUP_NOTIFICATION_TYPE = 'ADDED_TO_GROUP';
 const REMOVE_GROUP_NOTIFICATION_TYPE = 'REMOVED_FROM_GROUP';
 
-socket.listen(3000);
+socket.listen(5000);
 
 io.on('connection', function (clientSocket) {
 
